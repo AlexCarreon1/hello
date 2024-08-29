c3d7d08a2f02bfc8ce9daca2f48825e3bd996301
AlexCarreon1 <acarreon2@huskers.unl.edu>
2024-08-29 13:04:14 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**Author: Alex Carreon
+ *
+ * hello file prints My name
+ *
+ * Date: 09/29/24
+ *  */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Alex Carreon");
+    return 0;
+
+}
+
+
